708f57b8649c33306cafcf986943cf86f2edd9c7
Lisandro Veron <lisandro.veron@hotmail.com>
2023-08-08 21:16:32 -0300

    Client -> Added: 'App' component with socket connection and first socket emit event. CSS styles for 'App'. Borel and Lato font family.

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,8 +1,46 @@
+import {useEffect, useRef, useState} from "react";
 import { io } from "socket.io-client";
+import "./normalize.css";
+import "./App.css";
 
 export default function App(){
-	const socket = io();
+	const [message, setMessage] = useState("");
+	const [messagesList, setMessagesList] = useState([]);
+
+	const socket = useRef(null);
+
+	useEffect(() => {
+		socket.current = io({autoConnect:false});
+		socket.current.connect();
+
+		return () => {
+			socket.current.disconnect();
+		};
+	}, []);
+
+	function handleChange(e){
+		setMessage(e.target.value);
+	};
+
+	function handleSubmit(e){
+		e.preventDefault();
+		socket.current.emit("message", message);
+		setMessage("");
+		const input_message = document.getElementById("message");
+		input_message.value = "";
+	};
+
 	return <>
-		<p>Hello world.</p>
+		<header>
+			<img src="assets/favicon.svg" alt="" />
+			<h1>Mood Chat</h1>
+		</header>
+		<main>
+			<form onSubmit={handleSubmit}>
+				<input type="text" id="message" name="message" onChange={handleChange} />
+				<button>Send</button>
+			</form>
+			<p>{message}</p>
+		</main>
 	</>;
 };
