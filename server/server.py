35c4a69a3795c76597c3deb9e54f9bb9507aa2cc
Lisandro Veron <lisandro.veron@hotmail.com>
2023-08-11 00:55:18 -0300

    Server -> Added: 'dotenv' package for environment variables usage in CORS and socketio configuration. 'check' event for validate nickname before connect it to the chat, and a 'users' dictionary to store it. Modified: 'message' event now resend the broadcast message with the nickname sender attached.

--- server/server.py
+++ server/server.py
@@ -1,20 +1,45 @@
-from flask import Flask
+import os
+from dotenv import load_dotenv
+from flask import Flask, request
 from flask_cors import CORS
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO
 
+load_dotenv()
 app = Flask("server")
 CORS(app)
 socketio = SocketIO(
     app,
-    cors_allowed_origins="*")
+    cors_allowed_origins=os.environ["CORS_ORIGINS"])
+
+users = {}
 
 @app.route("/")
 def index():
     return ""
 
+@socketio.on("disconnect")
+def deleteUser():
+    if request.sid in users:
+        socketio.emit("message", f"{users[request.sid]} disconnected.")
+        del users[request.sid]
+        socketio.emit("usersUpdate", [nickname for nickname in users.values()])
+
 @socketio.on("message")
-def message(message):
-    socketio.emit("message", message)
+def transmit(message):
+    socketio.emit("message", f"{users[request.sid]}: {message}")
+
+@socketio.on("check")
+def check(nickname):
+    if nickname in users.values():
+        return False, "Nickname already in use."
+    users[request.sid] = nickname
+    socketio.emit("message", f"{nickname} connected.", include_self=False)
+    socketio.emit("usersUpdate", [nickname for nickname in users.values()])
+    return True, ""
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    socketio.run(
+        app,
+        host=os.environ["HOST"],
+        port=os.environ["PORT"],
+        debug=True)
